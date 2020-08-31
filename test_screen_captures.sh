3f68eed9ed0accbe0fa57032bc638cd633a49a5d
_ <a@b.co>
2020-08-31 20:05:58 +0530

    Much cleaner way to write tests

--- test_screen_captures.sh
+++ test_screen_captures.sh
@@ -3,6 +3,13 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
+BROOT_TMUX_SESSION="broot_test_$$"
+
+trap "tmux kill-session -t $BROOT_TMUX_SESSION; exit" SIGINT SIGTERM
+
+# BROOT_BINARY=/home/zx/workspace/git/broot/target/release/broot
+BROOT_BINARY=broot
+
 mkdir -p a/m/x a/m/y
 for i in $(seq 1 120)
 do
@@ -21,7 +28,7 @@ function capture_compare
 	echo -e "$YELLOW Test case $1 $DEFAULT"
 	CAPTURE_FILE="$CAPTURE_DIR/$1.capture"
 
-	tmux capture-pane -p -e > $CAPTURE_FILE
+	tmux capture-pane -p -e -t $BROOT_TMUX_SESSION > $CAPTURE_FILE
 	if ! diff $CAPTURE_FILE master/$1.master 
 	then
 		echo -e $RED "❌ $1"
@@ -32,71 +39,69 @@ function capture_compare
 	echo -e "$DEFAULT"
 }
 
+function SendKeysToTmuxSession
+{
+	tmux send-keys -t $BROOT_TMUX_SESSION $@
+	sleep 0.5	
+}
 
-tmux -u new-session -d -x 50 -y 50 -s broot_test
+tmux -u new-session -d -x 50 -y 50 -s $BROOT_TMUX_SESSION
+
 
 # Initial View
-tmux send -t broot_test 'unset RPROMPT' ENTER
-tmux send -t broot_test 'export PS1=$' ENTER
-tmux send -t broot_test clear ENTER
-tmux send -t broot_test broot ENTER
-sleep 0.5
+SendKeysToTmuxSession 'unset' SPACE 'RPROMPT' ENTER
+SendKeysToTmuxSession export SPACE PS1=$ ENTER
+SendKeysToTmuxSession clear ENTER
+SendKeysToTmuxSession $BROOT_BINARY ENTER
+
 capture_compare initial
 
 
 # Folder Pane, open
-tmux send-keys -t broot_test DOWN
-sleep 0.5
-tmux send-keys -t broot_test DOWN
-sleep 0.5
-tmux send-keys -t broot_test C-Right
-sleep 0.5
+SendKeysToTmuxSession DOWN
+SendKeysToTmuxSession DOWN
+SendKeysToTmuxSession C-Right
+
 capture_compare folder_pane_open
 
 # Folder Pane, closed
-tmux send-keys -t broot_test Escape
-sleep 0.5
+SendKeysToTmuxSession Escape
+
 capture_compare folder_pane_closed
 
 # Fuzzy file search
-tmux send-keys -t broot_test 112
-sleep 0.5
+SendKeysToTmuxSession 112
+
 capture_compare file_fuzzy
 
 # File contents
-tmux send-keys -t broot_test C-Right
-sleep 0.5
+SendKeysToTmuxSession C-Right
+
 capture_compare file_contents
 
-tmux send-keys -t broot_test Escape
-sleep 0.5
-tmux send-keys -t broot_test Escape
-sleep 0.5
+SendKeysToTmuxSession Escape
+SendKeysToTmuxSession Escape
 
 # Page down
-tmux send -t broot_test x
-sleep 0.5
-tmux send -t broot_test ENTER
-sleep 0.5
-tmux send -t broot_test PageDown
-sleep 0.5
-tmux send -t broot_test PageDown
-sleep 0.5
+SendKeysToTmuxSession x
+SendKeysToTmuxSession ENTER
+SendKeysToTmuxSession PageDown
+SendKeysToTmuxSession PageDown
+
 capture_compare page_down
 
 # Page up
-tmux send -t broot_test PageUp
-sleep 0.5
+SendKeysToTmuxSession PageUp
+
 capture_compare page_up
 
 # Print path
-tmux send -t broot_test :print_path ENTER
-sleep 0.5
+SendKeysToTmuxSession :print_path ENTER
+
 capture_compare print_path
 
-
 sleep 2
-tmux kill-session -t broot_test
+tmux kill-session -t $BROOT_TMUX_SESSION
 
 
 if $FAILURE
